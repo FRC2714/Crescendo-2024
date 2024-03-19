d1497d23271e3acec3b8029c04c03e2f98e5e348
Kamran Nag <kamrann2353@gmail.com>
2024-03-18 19:29:59 -0500

    added passing commands

--- src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
+++ src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -123,6 +123,20 @@ public class Superstructure extends SubsystemBase {
     return m_shooter.readySubwoofer();
   }
 
+    public Command readyPassToAmp() {
+    return m_shooter.readyPassToAmp();
+  }
+
+     public Command readySourceToMid() {
+    return m_shooter.readySourceToMid();
+  }
+
+      public Command readyUnderstage() {
+    return m_shooter.readyUnderstage();
+  }
+
+  
+
   public Command readyShooterToAmp() {
     return new ParallelCommandGroup(m_shooter.readyAmp(),
                                     m_amp.extend());
