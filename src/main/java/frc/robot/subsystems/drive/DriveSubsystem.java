d8b2fbf67b1867e6fb0de94048877f2ba6a1720d
kjn01 <91856362+kjn01@users.noreply.github.com>
2024-04-11 22:56:33 -0500

    set all max module speeds to 4.5 m/s

--- src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSubsystem.java
@@ -150,7 +150,7 @@ public class DriveSubsystem extends SubsystemBase {
             new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
                     new PIDConstants(4, 0.0, 0.0), // Translation PID constants
                     new PIDConstants(8, 0.0, 0.0), // Rotation PID constants
-                    6.77, // Max module speed, in m/s
+                    4.5, // Max module speed, in m/s
                     0.3706, // Drive base radius in meters. Distance from robot center to furthest module.
                     new ReplanningConfig() // Default path replanning config. See the API for the options here
             ),
